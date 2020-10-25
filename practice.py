83d9497bfc2a3bdeeb6d6ca06519c222325e3fd7
Sean Gurson <sdgurson@gmail.com>
2020-10-25 07:23:49 -0700

    Starting consecutive items kata

--- practice.py
+++ practice.py
@@ -1442,20 +1442,35 @@ words = "didn't C'HAPTER 1 I'nvestment versus Speculation: Results to Be Expecte
 # More examples in the test cases.
 
 # Good luck!
-import collections 
+# import collections 
 
-def repeats(arr):
-  counts = collections.Counter(arr)
-  sum = 0
-  nums = [num for num in counts if counts[num] == 1]
+# def repeats(arr):
+#   counts = collections.Counter(arr)
+#   sum = 0
+#   nums = [num for num in counts if counts[num] == 1]
 
-  return nums[0] + nums[1]
+#   return nums[0] + nums[1]
 
-print(repeats([4,5,7,5,4,8]))
-print(repeats([9, 10, 19, 13, 19, 13]))
-print(repeats([16, 0, 11, 4, 8, 16, 0, 11]))
-print(repeats([5, 17, 18, 11, 13, 18, 11, 13]))
-print(repeats([5, 10, 19, 13, 10, 13]))
+# print(repeats([4,5,7,5,4,8]))
+# print(repeats([9, 10, 19, 13, 19, 13]))
+# print(repeats([16, 0, 11, 4, 8, 16, 0, 11]))
+# print(repeats([5, 17, 18, 11, 13, 18, 11, 13]))
+# print(repeats([5, 10, 19, 13, 10, 13]))
 
+# kata consecutive items
+# You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise). It is guaranteed that a and b are both present in arr.
 
+# get the index of a and b and find their difference, if difference is 1 or -1, return true, else return false
+def consecutive(arr, a, b):
+  print(arr.index(b)) 
 
+  for i,num in enumerate(arr):
+    # print(i, num)
+    pass
+
+consecutive([1, 3, 5, 7], 3, 7)
+# False
+consecutive([1, 3, 5, 7], 3, 1)
+# True
+consecutive([1, 6, 9, -3, 4, -78, 0], -3, 4)
+# True
