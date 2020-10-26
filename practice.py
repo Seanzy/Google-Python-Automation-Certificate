db264989b9a34fbf0a357e8b40b2dfce6ed0ec40
Sean Gurson <sdgurson@gmail.com>
2020-10-26 13:20:54 -0700

    Passing consecutive items kata

--- practice.py
+++ practice.py
@@ -1461,16 +1461,13 @@ words = "didn't C'HAPTER 1 I'nvestment versus Speculation: Results to Be Expecte
 # You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise). It is guaranteed that a and b are both present in arr.
 
 # get the index of a and b and find their difference, if difference is 1 or -1, return true, else return false
-def consecutive(arr, a, b):
-  print(arr.index(b)) 
+# def consecutive(arr, a, b):
+#   return abs(arr.index(a) - arr.index(b)) == 1
 
-  for i,num in enumerate(arr):
-    # print(i, num)
-    pass
 
-consecutive([1, 3, 5, 7], 3, 7)
-# False
-consecutive([1, 3, 5, 7], 3, 1)
-# True
-consecutive([1, 6, 9, -3, 4, -78, 0], -3, 4)
-# True
+# print(consecutive([1, 3, 5, 7], 3, 7))
+# # False
+# print(consecutive([1, 3, 5, 7], 3, 1))
+# # True
+# print(consecutive([1, 6, 9, -3, 4, -78, 0], -3, 4))
+# # True
